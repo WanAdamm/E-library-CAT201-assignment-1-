6ab3cad14de502278bb65a0c0a66176e19d80f16
WanAdamm <wanadamworkmail@gmail.com>
2024-11-24 19:22:35 +0800

    added some functionality to the Library class

--- src/main/java/com/example/Library.java
+++ src/main/java/com/example/Library.java
@@ -1,30 +1,40 @@
 package com.example;
 
 import java.util.ArrayList;
-import com.example.Book;
 
 public class Library {
-    
-    private ArrayList<Book> bookList = new ArrayList <>();
 
-    public void addBooks(String imgURL, String title, String author, String ISBN) 
-    {
+    private ArrayList<Book> bookList = new ArrayList<>();
+
+    public void addBooks(String imgURL, String title, String author, String ISBN) {
         Book book = new Book(imgURL, title, author, ISBN, " "); // No borrower at initialization
-        bookList.add(book); 
+        bookList.add(book);
     }
 
-    // public Book searchByTitle()
-    // {
-    //     return Book;
-    // }
+    public Book searchByTitle(String title) {
+        for (Book book : bookList) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null; // If no book with the given title is found
+    }
 
-    // public Book searchByAuthor()
-    // {
-        
-    // }
+    public Book searchByAuthor(String author) {
+        for (Book book : bookList) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
+                return book;
+            }
+        }
+        return null; // If no book with the given author is found
+    }
 
-    // public Book searchByISBN()
-    // {
-        
-    // }
+    public Book searchByISBN(String ISBN) {
+        for (Book book : bookList) {
+            if (book.getISBN().equals(ISBN)) {
+                return book;
+            }
+        }
+        return null; // If no book with the given ISBN is found
+    }
 }
